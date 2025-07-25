81db2d68231d29052b090c63c7f9083282780795
ravidab <ravidab>
2025-07-25 11:53:13 +0000

    commit

---
+++ src/create_bronze_table.py
@@ -0,0 +1,21 @@
+# Databricks notebook source
+my_catalog = dbutils.widgets.get('catalog_name')
+target = dbutils.widgets.get('display_target')
+
+set_default_catalog = spark.sql(f'USE CATALOG {my_catalog}')
+
+print(f'Using the {my_catalog} catalog.')
+print(f'Deploying as the {target} pipeline.')
+
+# COMMAND ----------
+
+spark.sql(f'''
+CREATE OR REPLACE TABLE {my_catalog}.default.Bronze_health_1 AS
+SELECT 
+  *
+FROM read_files(
+  '/Volumes/dabdemo-dev/default/health',
+  format => 'csv',
+  header => true
+)
+''')
---
+++ src/create_silver_table.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+my_catalog = dbutils.widgets.get('catalog_name')
+target = dbutils.widgets.get('display_target')
+
+set_default_catalog = spark.sql(f'USE CATALOG {my_catalog}')
+
+print(f'Using the {my_catalog} catalog.')
+print(f'Deploying as the {target} pipeline.')
+
+# COMMAND ----------
+
+# Create or replace the silver table from bronze table
+spark.sql(f"""
+CREATE OR REPLACE TABLE {my_catalog}.default.silver_health_1 AS
+SELECT * 
+FROM {my_catalog}.default.Bronze_health_1
+""")
